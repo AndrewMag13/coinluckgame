dbb44206e23897926feac114d70ef878e326e3f2
AndrewMag13 <nnaaiillburdasov@gmail.com>
2021-11-08 07:17:04 +0300

    removed useless files

--- flask/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, PasswordField
- 
-class LoginForm(FlaskForm):
-    email = StringField("Email: ")
-    psw = StringField("Пароль: ")
-    submit = SubmitField("Войти")
